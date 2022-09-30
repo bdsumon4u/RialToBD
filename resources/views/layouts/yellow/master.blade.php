7135e69d85e8a0f04227c0e830e21441b88dc7be
Sumon Ahmed <75123992+bdsumon4u@users.noreply.github.com>
2022-09-30 11:33:32 +0600

    TiDiO

--- resources/views/layouts/yellow/master.blade.php
+++ resources/views/layouts/yellow/master.blade.php
@@ -196,6 +196,7 @@
             }
         </script>
     @endif
+    <script src="//code.tidio.co/rlmurwkkffbeqmlwv11wejlqjgd0irtl.js" async></script>
 </head>
 
 <body class="header-fixed" style="margin: 0; padding: 0;">
